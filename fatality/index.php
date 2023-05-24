7519b3aa40f1e09d7036ba09a0239c0ddb773dda
Baxadir_Jalalov <125832459+BaxadirJalalov@users.noreply.github.com>
2023-05-24 15:31:48 +0500

    Add files via upload

---
+++ fatality/index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Fatality средство для похудения</title>
+	    <!-- Favicon -->
+    <link rel="icon" href="img/products_new/8828.png" type="image/png">
+    <link rel="shortcut icon" href="img/products_new/8828.png" type="image/png">
+
+    <!-- OG -->
+    <meta property="og:url" content="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Fatality средство для похудения" />
+    <meta property="og:image" content="http://<?php echo $_SERVER["HTTP_HOST"]; ?>/img/products_new/8828.png" />
+    <meta property="og:image:width" content="150" />
+    <meta property="og:image:height" content="150" />
+    <meta property="og:site_name" content="Fatality средство для похудения" />
+    <meta property="og:description" content="Fatality средство для похудения" />
+
+    
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta content="width=480" name="viewport">
+	
+	<!-- CSS -->
+	<link type="text/css" rel="stylesheet" href="files/fatality_UZ_mobile_2/css/style.css" />
+	
+	<!-- JS -->
+	<script src="files/_js/jquery-1.10.2.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="files/_js/validation.js"></script>
+	<script type="text/javascript" src="files/_js/ab.min.js"></script>
+	<script type="text/javascript" src="files/_js/lastpack.js"></script>
+
+  	<script type="text/javascript" src="files/fatality_UZ_mobile_2/js/slick.min.js"></script>
+  	<script type="text/javascript" src="files/fatality_UZ_mobile_2/js/script.js"></script>
+	<script type="text/javascript">$jsonData = {"product":{"id":8828,"name":"Fatality средство для похудения 33000 - UZ (Узбекистан) USD","title":"Fatality средство для похудения","short_description":"Fatality средство для похудения","full_description":"Fatality средство для похудения"},"prices":{"481":{"price":33000,"old_price":200000,"delivery_price":0,"tax_price":0,"upsale_price":0,"geo_key":"UZ","name":"Uzbekistan","currency":"UZS","rate":46.62,"phone_template":"+998 123123123","address_template":"","name_template":"Anvar","active":true}},"lowPrice":{"year":"2023","month":"05","day":"26"}};</script>
+</head>
+<body><script type="text/javascript">
+  var landing_notifiers = new Object(),
+    price, currency, delivery_price;
+
+  for (key in $jsonData.prices) {
+    if ($jsonData.prices[key].active) {
+      price = $jsonData.prices[key].price;
+      currency = $jsonData.prices[key].currency;
+      delivery_price = $jsonData.prices[key].price + $jsonData.prices[key].delivery_price;
+    }
+  }
+
+  landing_notifiers.params = {
+    city: "",
+    landDir: "fatality_UZ_mobile_2",
+    sex: "",
+    multiply: "" || 0,
+    no_price: "" || false,
+    delay: ("" * 1000) || 12000,
+    price: price || 0,
+    currency: currency || "руб.",
+    delivery_price: delivery_price,
+    geoKey: "" || "RU",
+    zdorov: "" || false
+  };
+</script>
+
+<script type="text/javascript" src="files/_blocks/notificators/func.js?11"></script>
+
+<script>
+  var formIsSubmitted = false;
+</script>
+
+<script type="text/javascript" src="files/_blocks/notificators/unload_submit.js?7"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                              
+            
+    <style>
+        .block-1 .container {
+            background: url(/files/fatality_UZ_mobile_2/img/block1_caps.png) center 0 no-repeat #103963;
+        }
+        .block-11 .container {
+            background: url(/files/fatality_UZ_mobile_2/img/block1_caps.png) center 0 no-repeat #103963;
+        }
+    </style>
+
+<div class="main-wrap">
+    <div class="section block-1">
+        <h1> Fatality Жирагон </h1>
+        <p><span style="color:#f3045e"> 14 KUN DAVOMIDA </span> XAVFSIZ VAZN YO'QOTISH <span style='display:none'>90861c1c2f</span></p>
+        <div class="container">
+            <div class="price">
+                <div class="old-price"> AKSIYA BO'YICHA NARX <br><s> 200000 <small> UZS </small></s></div>
+                                    <div class="new"> 33000 <small> UZS </small></div>
+                
+            </div><a class="button-m scrool" href="#form"> Buyurtma </a>
+            <div style="margin: 15px 20px 0;
+text-align: center;
+"> (ANONIM ETKAZIB BERISH) </div>
+        </div>
+    </div>
+    <div class="section block-2">
+        <ul class="edit-sp">
+            <li> - Xavfsiz vazn yo'qotish </li>
+            <li> 100% tabiiy tarkibi </li>
+            <li> Yurak va asab tizimiga ta'sir qilmaydi </li>
+            <li> Yurak va asab tizimiga ta'sir qilmaydi </li>
+        </ul>
+    </div>
+    <div class="section block-3">
+        <h2> NIMA UCHUN VAZN ORTTIRYAPSIZ? </h2><img alt="весы" src="files/fatality_UZ_mobile_2/img/img1.jpg">
+        <p> Do'konda sotib olgan barcha mahsulotlar (go'sht, sut, sabzavot) sun'iy konservantlar,aromatizatorlar, lazzat kuchaytirgichlar va bo'yoqlarni o'z ichiga oladi. Ushbu kimyoviy tarkibiy qismlar ovqat hazm qilish muammolari, yog'larning notekis birikishi, shlakli turg'unlik va boshqa muammolarni keltirib chiqaradi. </p>
+    </div>
+    <div class="section block-4">
+        <h2> FATALITY NIMA? </h2>
+        <ul>
+            <li> Fatality-100% tabiiy va ekologik toza mahsulot. Kimyoviy va sun'iy sintezlangan moddalar yo'q! </li>
+            <li> Sizning kelajakdagi natijangiz uzoq kuttirmaydi. Faqat 1 Fatality
+
+                                    kapsulalar
+                
+                - va sizning qomatingiz kundan - kunga o'zgaradi! </li>
+            <li> Fatality butunlay xavfsiz. Bu yurak-qon tomir va Markaziy asab tizimiga ta'sir qilmaydi </li>
+        </ul>
+    </div>
+    <div class="section block-5">
+        <h2> Fatality fatality zarur vitaminlar va iz elementlarni o'z ichiga oladi </h2>
+        <ul class="edit-sp clearfix">
+            <li> Yantar kislotasi </li>
+            <li> Guarana ekstrakti suv o'tlari </li>
+            <li> Suv o'tlari fuchus ekstrakti </li>
+            <li> Kofein </li>
+            <li> Chitosan </li>
+            <li> Coleus forskolia ekstrakti </li>
+            <li> L-KARNITIN </li>
+            <li> Vitaminlar V2, V5, B6, B8, B12 </li>
+        </ul>
+    </div>
+    <div class="section block-6">
+        <h2> BUGUNGI KUNDA FATALITY-NI 84% CHEGIRMA BILAN BUYURTMA QILIN </h2><a class="button-m scrool" href="#form"> BUYURTMA QILISH </a>
+    </div>
+    <div class="section block-7">
+        <h2> DIETOLOGNING FIKRI </h2><img alt="доктор" src="files/fatality_UZ_mobile_2/img/ava-doc.jpg">
+        <p><b> Fatality Evropa texnologiyasi asosida ishlab chiqarilgan va oziq-ovqat va dori sifati bo'yicha sanitariya nazorati tomonidan tasdiqlangan </b></p>
+        <p><span style='display:none'>f1bb5884f5</span> «Shunday qilib, tanangiz o'zini tozalashni boshlaydi, ovqatdan oldin bir
+
+                            kapsula
+            
+            ichasiz, kuniga 3 marta. Siz tezda farqni sezasiz-bir hafta ichida vazn pasayib ketadi, oshqozon va oshqozon muammolari yo'qoladi. Tabiiy vitamin komplekslari har qanday kimyoviy birikmalarga qaraganda ancha samarali va xavfsizdir. </p>
+    </div>
+    <div class="section block-8">
+        <h2> FATALITY QANDAY ISHLAYDI? </h2>
+        <ul class="edit-sp clearfix">
+            <li> YOG'LARNI ERITADI </li>
+            <li> TANANI TOZALAYDI </li>
+            <li> YOG ' BIRIKMASINI BLOKLAYDI </li>
+            <li> QARISH JARAYONINI SEKINLASHTIRADI </li>
+        </ul>
+    </div>
+    <div class="section block-9">
+        <h2> FATALITY QABUL QILISH TUFAYLI ULAR ALLAQACHON VAZN YO'QOTISHDI </h2>
+        <div class="slide-vk">
+            <div><img alt="Отзыв с Fb" src="files/fatality_UZ_mobile_2/img/vk1.jpg"></div>
+            <div><img alt="Отзыв с Fb" src="files/fatality_UZ_mobile_2/img/vk2.jpg"></div>
+            <div><img alt="Отзыв с Fb" src="files/fatality_UZ_mobile_2/img/vk3.jpg"></div>
+        </div>
+    </div>
+    <div class="section block-10">
+        <h2> FATALITY-GA QANDAY BUYURTMA BERISH MUMKIN? </h2>
+        <ul class="edit-sp clearfix">
+            <li> BUYURTMA </li>
+            <li> BUYURTMA BERISH </li>
+            <li> MAHSULOT YETKAZIB BERISH </li>
+            <li> QABUL VA TO'LOV </li>
+        </ul>
+    </div>
+    <div class="section block-11">
+        <h2> BUGUNGI KUNDA "FATALITY" NI 84% CHEGIRMA BILAN BUYURTMA QILING </h2>
+        <div class="container">
+            <div class="price">
+                <div class="old-price"> AKSIYA BO'YICHA NARX <br><s> 200000 <small> UZS </small></s></div>
+                                    <div class="new"> 33000 <small> UZS </small></div>
+                            </div>
+        </div>
+    </div>
+    <div class="section block-12" id="form">
+        <div class="timer">
+            <p class="center"> Aksiya tugaguniga qadar: </p>
+            <div class="countbox clearfix"></div>
+        </div>
+
+        <img src="files/fatality_UZ_mobile_2/img/image.png"/>
+
+        <img src="files/fatality_UZ_mobile_2/img/d.jpg">
+        <div style="padding: 30px 20px 0;
+background: #323c57;
+text-align: center;
+margin-top: -7px;
+border-top: 1px solid #fd2174;
+"> Yetkazib berishni to'liq anonimligini kafolatlaymiz. </div>
+        <form class="main-order-form m1-form order-form orderformcdn" action="order.php" method="post">
+<input type="hidden" name="sub1" value="<?php echo !empty($_GET['utm_source']) ? htmlspecialchars($_GET['utm_source']) : ''; ?>">
+<input type="hidden" name="sub2" value="<?php echo !empty($_GET['utm_medium']) ? htmlspecialchars($_GET['utm_medium']) : ''; ?>">
+<input type="hidden" name="sub3" value="<?php echo !empty($_GET['utm_campaign']) ? htmlspecialchars($_GET['utm_campaign']) : ''; ?>">
+<input type="hidden" name="sub4" value="<?php echo !empty($_GET['utm_content']) ? htmlspecialchars($_GET['utm_content']) : ''; ?>">
+<input type="hidden" name="sub5" value="<?php echo !empty($_GET['utm_term']) ? htmlspecialchars($_GET['utm_term']) : ''; ?>">
+<input type="hidden" name="fbpx" value="<?php echo !empty($_GET['fbpx']) ? htmlspecialchars($_GET['fbpx']) : ''; ?>">
+
+
+            <input name="total_price" type="hidden" value="33000">
+            <input name="total_price_wo_shipping" type="hidden" value="33000">
+            <input name="price" type="hidden" value="33000">
+            <input name="old_price" type="hidden" value="200000">
+            <input name="currency" type="hidden" value="UZS">
+
+            <div class="message"></div>
+            <div>
+                                    <select name="country" id='country' class="field__select field__part country_select">  <option value='481' selected="selected">Uzbekistan</option>                     </select>
+                
+                <input type="hidden" name="address" value="">
+                <input type="hidden" name="product_count" id="product_count" value="1">
+                <input type="hidden" name="number_product" value="8828">
+
+                <input type="hidden" name="split_test_id" 	value="0">
+<input type="hidden" name="split_test" 		value="0">
+<input type="hidden" name="split_test_host" value="">
+<input type="hidden" name="fchck"           value="f850f269a7ce0b5784c683e8de1f5037c6dcb8f2">
+<input type="hidden" name="ucfi" id="ucfi"  value="0">            </div>
+            <div><input name="name" placeholder="I.F.O kiriting" type="text"></div>
+            <div><input class="only_number" name="phone" placeholder="Telefonni kiriting" type="text"></div>
+            <div><button class="submitBtn button-m js_submit" style="color: #fff;font-size: 30px;"> Buyurtma kirish </button>
+                <div class="stock"> Aksiya bo’yicha <span> <span class="lastpack"></span> <span style='display:none'>54628bdc04</span></span> dona qoldi </div>
+            </div>
+                        <div class="copyright">
+                Не является лекарственным средством.
+
+<img src="files/_blocks/copyright/img/rekv.png" style="display:block;margin:0 auto">
+
+<div style="text-align: right"><img src="files/_blocks/copyright/img/1001transits.png" style="display: inline-block !important;" /></div><br>
+                <a href="privacypolicy" target="_blank">Политика конфиденциальности</a>
+            </div>
+        </form>
+    </div>
+</div>
+
+	
+
+
+<?php if (!empty($_GET['fbpx'])): ?><!-- Facebook Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,"script","https://connect.facebook.net/en_US/fbevents.js");fbq("init", "<?php echo htmlspecialchars($_GET['fbpx']); ?>");fbq("track", "PageView");</script><noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?php echo htmlspecialchars($_GET['fbpx']); ?>&ev=PageView&noscript=1"/></noscript><!-- End Facebook Pixel Code --><?php endif; ?>
+</body>
+</html>
+
+
+
+
